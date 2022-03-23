7e415599ffb43bb5c9cda6ec5583a6bd651f5c86
Esraa Elkabeer <esraaelkaber18@gmail.com>
2022-03-23 02:01:52 +0200

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require('cors')
 require("dotenv").config();
 
 const userRouter = require("./Modules/Users/UserRoutes");
@@ -8,6 +9,7 @@ const movieRouter = require("./Modules/Movies/MovieRoutes")
 
 const port = 3000;
 app.use(express.json());
+app.use(cors())
 
 app.use(function (req, res, next) {
 	const allowedOrigins = [
