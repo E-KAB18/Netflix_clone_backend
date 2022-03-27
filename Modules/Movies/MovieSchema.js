bf23f2fff8ecc2721a3dfe576a3bccd94fc0808b
Nada Elsnosy <nadaalsnosy@gmail.com>
2022-03-28 00:48:53 +0200

    add userlist

--- Modules/Movies/MovieSchema.js
+++ Modules/Movies/MovieSchema.js
@@ -1,50 +1,53 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const MovieSchema = new Schema({
-    title: {
-        type: String,
-        required: true, unique: true
-    },
-    desc: {
-        type: String,
-    },
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  desc: {
+    type: String,
+  },
 
-    img: {
-        type: String,
-    },
-    trailer: {
-        type: String,
-    },
+  img: {
+    type: String,
+  },
+  trailer: {
+    type: String,
+  },
 
-    video: {
-        type: String,
-    },
+  video: {
+    type: String,
+  },
 
-    rate: {
-        type: String,
-    },
+  rate: {
+    type: Number,
+  },
 
-    year: {
-        type: Date
-    },
-    limit: {
-        type: String
-    },
-    genere: {
-        type: String,
-        enum: ["Action",
-            "Comedy",
-            "Drama",
-            "Fantasy",
-            "Horror",
-            "Mystery",
-            "Romance",
-            "Thriller"]
-    },
-    isSeries: {
-        type: Boolean,
-        default: false,
-    },
+  year: {
+    type: Date,
+  },
+  limit: {
+    type: String,
+  },
+  genere: {
+    type: String,
+    enum: [
+      "Action",
+      "Comedy",
+      "Drama",
+      "Fantasy",
+      "Horror",
+      "Mystery",
+      "Romance",
+      "Thriller",
+    ],
+  },
+  isSeries: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = { MovieSchema };
--- Modules/Users/UserControllers.js
+++ Modules/Users/UserControllers.js
@@ -6,100 +6,100 @@ const User = require("./UserModel");
 const asynSign = util.promisify(jwt.sign);
 
 const signUp = async (req, res, next) => {
-	const { username, email, password } = req.body;
-	try {
-		const newUser = new User({ username, email, password });
-		if (req.file) {
-			newUser.image = req.file.path;
-		}
-		const createdUser = await newUser.save();
-		res.send(createdUser);
-	} catch (error) {
-		error.statusCode = 500;
-		next(error);
-	}
+  const { username, email, password } = req.body;
+  try {
+    const newUser = new User({ username, email, password });
+    if (req.file) {
+      newUser.image = req.file.path;
+    }
+    const createdUser = await newUser.save();
+    res.send(createdUser);
+  } catch (error) {
+    error.statusCode = 500;
+    next(error);
+  }
 };
 
 const login = async (req, res, next) => {
-	const { email, password } = req.body;
-	try {
-		const user = await User.findOne({ email });
-		if (!user) throw new Error("invalid email or password");
-		const { password: originalHashedPassword } = user;
-		const result = await bcrypt.compare(password, originalHashedPassword);
-		if (!result) throw new Error("invalid email or password");
-		const { id: uID } = user;
-		const token = await asynSign(
-			{ id: uID, isAdmin: user.isAdmin },
-			process.env.SECRET_KEY
-		);
-		// { expiresIn: process.env.JWT_EXPIRES_IN }
-		res.send({ token, user });
-	} catch (error) {
-		error.statusCode = 500;
-		next(error);
-	}
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("invalid email or password");
+    const { password: originalHashedPassword } = user;
+    const result = await bcrypt.compare(password, originalHashedPassword);
+    if (!result) throw new Error("invalid email or password");
+    const { id: uID } = user;
+    const token = await asynSign(
+      { id: uID, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY
+    );
+    // { expiresIn: process.env.JWT_EXPIRES_IN }
+    res.send({ token, user });
+  } catch (error) {
+    error.statusCode = 500;
+    next(error);
+  }
 };
 
 const getUsers = async (req, res, next) => {
-	try {
-		if (req.userPayload.isAdmin) {
-			const users = await User.find();
-			res.send(users);
-		} else {
-			throw new Error(`You are not allowed to see all users!`);
-		}
-	} catch (error) {
-		error.statusCode = 403;
-		next(error);
-	}
+  try {
+    if (req.userPayload.isAdmin) {
+      const users = await User.find();
+      res.send(users);
+    } else {
+      throw new Error(`You are not allowed to see all users!`);
+    }
+  } catch (error) {
+    error.statusCode = 403;
+    next(error);
+  }
 };
 const profile = async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		if (req.userPayload.id === id) {
-			const { username, email } = await User.findById(id);
-			res.send({ username, email });
-		} else {
-			throw new Error(`You are not allowed to view this profile!`);
-		}
-	} catch (error) {
-		error.statusCode = 403;
-		next(error);
-	}
+  try {
+    const { id } = req.params;
+    if (req.userPayload.id === id) {
+      const { username, email } = await User.findById(id);
+      res.send({ username, email });
+    } else {
+      throw new Error(`You are not allowed to view this profile!`);
+    }
+  } catch (error) {
+    error.statusCode = 403;
+    next(error);
+  }
 };
 
 const updateUser = async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const { username, email, password } = req.body;
-		if (req.userPayload.id === id || req.userPayload.isAdmin) {
-			const updatedUsers = await User.findByIdAndUpdate(
-				id,
-				{ username, email, password },
-				{ new: true }
-			);
-			res.send(updatedUsers);
-		} else {
-			throw new Error(`You are not allowed to update this profile!`);
-		}
-	} catch (error) {
-		error.statusCode = 403;
-		next(error);
-	}
+  try {
+    const { id } = req.params;
+    const { username, email, password, userList } = req.body;
+    if (req.userPayload.id === id || req.userPayload.isAdmin) {
+      const updatedUsers = await User.findByIdAndUpdate(
+        id,
+        { username, email, password, userList },
+        { new: true }
+      );
+      res.send(updatedUsers);
+    } else {
+      throw new Error(`You are not allowed to update this profile!`);
+    }
+  } catch (error) {
+    error.statusCode = 403;
+    next(error);
+  }
 };
 const deleteUser = async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		if (req.userPayload.id === id || req.userPayload.isAdmin) {
-			const deletedUser = await User.findByIdAndDelete(id);
-			res.send(`${deletedUser.username} is deleted successfully`);
-		} else {
-			throw new Error(`You are not allowed to delete this profile!`);
-		}
-	} catch (error) {
-		error.statusCode = 403;
-		next(error);
-	}
+  try {
+    const { id } = req.params;
+    if (req.userPayload.id === id || req.userPayload.isAdmin) {
+      const deletedUser = await User.findByIdAndDelete(id);
+      res.send(`${deletedUser.username} is deleted successfully`);
+    } else {
+      throw new Error(`You are not allowed to delete this profile!`);
+    }
+  } catch (error) {
+    error.statusCode = 403;
+    next(error);
+  }
 };
 module.exports = { signUp, login, getUsers, profile, updateUser, deleteUser };
